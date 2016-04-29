fd2a9a91ccbc0da30a8a33910dab2338ca7c41dc
Jonas Keunecke <keunecke@cscp.eu>
2016-04-29 19:45:00 +0200

    Fixed space!

--- src/main/java/su/litvak/chromecast/api/v2/Volume.java
+++ src/main/java/su/litvak/chromecast/api/v2/Volume.java
@@ -77,7 +77,7 @@ public class Volume {
     }
 
     @Override
-    public boolean equals(final Object obj) {
+    public boolean equals (final Object obj) {
         if (obj == null) return false;
         if (obj == this) return true;
         if (!(obj instanceof Volume))  return false;
