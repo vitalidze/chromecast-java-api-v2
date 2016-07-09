cdd5b2271717f9cc2ecff14a04cb96f7b6f40446
Jorge Ruesga <jorge@ruesga.com>
2016-07-09 17:22:32 +0200

    Fix typo
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/main/java/su/litvak/chromecast/api/v2/AppEvent.java
+++ src/main/java/su/litvak/chromecast/api/v2/AppEvent.java
@@ -18,7 +18,7 @@ package su.litvak.chromecast.api.v2;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 /**
- * An custom event sent by a receiver app.
+ * A custom event sent by a receiver app.
  */
 public class AppEvent {
     @JsonProperty
