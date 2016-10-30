99250880bb0ba0246abb18248fc11df52cc51ba9
Vitaly Litvak <vitavaque@gmail.com>
2016-10-30 15:22:23 +0300

    For #51 - throw exception in methods, which expect ChromeCast to be running some application

--- src/main/java/su/litvak/chromecast/api/v2/ChromeCast.java
+++ src/main/java/su/litvak/chromecast/api/v2/ChromeCast.java
@@ -153,12 +153,18 @@ public class ChromeCast {
     }
 
     /**
-     * Stops currently running application
+     * <p>Stops currently running application</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
      *
      * @throws IOException
      */
     public void stopApp() throws IOException {
-        channel.stop(getRunningApp().sessionId);
+        Application runningApp = getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        channel.stop(runningApp.sessionId);
     }
 
     /**
@@ -178,55 +184,84 @@ public class ChromeCast {
     }
 
     /**
+     * <p>If no application is running at the moment then exception is thrown.</p>
+     *
      * @return current media status, state, time, playback rate, etc.
      * @throws IOException
      */
     public MediaStatus getMediaStatus() throws IOException {
-        return channel.getMediaStatus(getRunningApp().transportId);
+        Application runningApp = getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        return channel.getMediaStatus(runningApp.transportId);
     }
 
     /**
-     * Resume paused media playback
+     * <p>Resume paused media playback</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
+     *
      * @throws IOException
      */
     public void play() throws IOException {
         Status status = getStatus();
-        MediaStatus mediaStatus = channel.getMediaStatus(status.getRunningApp().transportId);
+        Application runningApp = status.getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        MediaStatus mediaStatus = channel.getMediaStatus(runningApp.transportId);
         if (mediaStatus == null) {
             throw new ChromeCastException("ChromeCast has invalid state to resume media playback");
         }
-        channel.play(status.getRunningApp().transportId, status.getRunningApp().sessionId, mediaStatus.mediaSessionId);
+        channel.play(runningApp.transportId, runningApp.sessionId, mediaStatus.mediaSessionId);
     }
 
     /**
-     * Pause current playback
+     * <p>Pause current playback</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
+     *
      * @throws IOException
      */
     public void pause() throws IOException {
         Status status = getStatus();
-        MediaStatus mediaStatus = channel.getMediaStatus(status.getRunningApp().transportId);
+        Application runningApp = status.getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        MediaStatus mediaStatus = channel.getMediaStatus(runningApp.transportId);
         if (mediaStatus == null) {
             throw new ChromeCastException("ChromeCast has invalid state to pause media playback");
         }
-        channel.pause(status.getRunningApp().transportId, status.getRunningApp().sessionId, mediaStatus.mediaSessionId);
+        channel.pause(runningApp.transportId, runningApp.sessionId, mediaStatus.mediaSessionId);
     }
 
     /**
-     * Moves current playback time point to specified value
+     * <p>Moves current playback time point to specified value</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
+     *
      * @param time time point between zero and media duration
      * @throws IOException
      */
     public void seek(double time) throws IOException {
         Status status = getStatus();
-        MediaStatus mediaStatus = channel.getMediaStatus(status.getRunningApp().transportId);
+        Application runningApp = status.getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        MediaStatus mediaStatus = channel.getMediaStatus(runningApp.transportId);
         if (mediaStatus == null) {
             throw new ChromeCastException("ChromeCast has invalid state to seek media playback");
         }
-        channel.seek(status.getRunningApp().transportId, status.getRunningApp().sessionId, mediaStatus.mediaSessionId, time);
+        channel.seek(runningApp.transportId, runningApp.sessionId, mediaStatus.mediaSessionId, time);
     }
 
     /**
-     * Loads and starts playing media in specified URL
+     * <p>Loads and starts playing media in specified URL</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
      *
      * @param url    media url
      * @return The new media status that resulted from loading the media.
@@ -237,7 +272,9 @@ public class ChromeCast {
     }
 
     /**
-     * Loads and starts playing specified media
+     * <p>Loads and starts playing specified media</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
      *
      * @param title name to be displayed
      * @param thumb url of video thumbnail to be displayed, relative to media url
@@ -248,14 +285,20 @@ public class ChromeCast {
      */
     public MediaStatus load(String title, String thumb, String url, String contentType) throws IOException {
         Status status = getStatus();
+        Application runningApp = status.getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
         Map<String, Object> metadata = new HashMap<String, Object>(2);
         metadata.put("title", title);
         metadata.put("thumb", thumb);
-        return channel.load(status.getRunningApp().transportId, status.getRunningApp().sessionId, new Media(url, contentType, null, null, null, metadata, null, null), true, 0d, null);
+        return channel.load(runningApp.transportId, runningApp.sessionId, new Media(url, contentType, null, null, null, metadata, null, null), true, 0d, null);
     }
 
     /**
-     * Loads and starts playing specified media
+     * <p>Loads and starts playing specified media</p>
+     *
+     * <p>If no application is running at the moment then exception is thrown.</p>
      *
      * @param media The media to load and play.  See https://developers.google.com/cast/docs/reference/messages#Load for further details.
      * @return The new media status that resulted from loading the media.
@@ -263,7 +306,11 @@ public class ChromeCast {
      */
     public MediaStatus load(final Media media) throws IOException {
         Status status = getStatus();
-        return channel.load(status.getRunningApp().transportId, status.getRunningApp().sessionId, media, true, 0d, null);
+        Application runningApp = status.getRunningApp();
+        if (runningApp == null) {
+            throw new ChromeCastException("No application is running in ChromeCast");
+        }
+        return channel.load(runningApp.transportId, runningApp.sessionId, media, true, 0d, null);
     }
 
     /**
