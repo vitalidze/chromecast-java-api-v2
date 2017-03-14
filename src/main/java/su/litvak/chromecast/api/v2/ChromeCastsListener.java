20506b1bbf0a2024229eee5816b077f07079e692
Jason Holmes <holmes@squareup.com>
2017-03-13 21:47:19 -0700

    Consistent parameter naming in ChromeCastsListener

--- src/main/java/su/litvak/chromecast/api/v2/ChromeCastsListener.java
+++ src/main/java/su/litvak/chromecast/api/v2/ChromeCastsListener.java
@@ -22,6 +22,6 @@ public interface ChromeCastsListener {
 
     void newChromeCastDiscovered(ChromeCast chromeCast);
 
-    void chromeCastRemoved(ChromeCast chromecast);
+    void chromeCastRemoved(ChromeCast chromeCast);
 
 }
